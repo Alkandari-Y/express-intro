043a605e3323c5072eac7b41c7b63918bad54478
Alkandari-Y <yayalkandari@gmail.com>
2021-10-24 22:41:59 +0300

    task done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const data = require("./data")
 const app = express();
-
+app.use(express.json())
+let data = require("./data")
 
 app.get("/", (req, res) => {
     res.json({task: "done"})
@@ -11,6 +11,27 @@ app.get("/api/products", (req, res) => {
     res.json(data)
 })
 
+app.post("/api/products", (req, res) => {
+    data.push(req.body)
+    res.status(200).json(data)
+})
+
+app.delete("/api/products/:productId", (req, res) => {
+    const reqId = +req.params.productId
+    let search = data.find(product => +product.id === reqId)
+    try{
+        if (!search) res.status(400).json('There nothing to delete')
+        if (search){
+            let tempData = data.filter(product => product.id !== reqId)
+            data = tempData
+            res.status(204).json(data)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+console.log('outside the function',data)
 const PORT = 8000;
 
 app.listen(PORT, () => {
